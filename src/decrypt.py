81593be51cf56435b236f4bee265d2021386c323
Carlos Castaneda <carlosdevmail@gmail.com>
2025-02-27 19:34:46 -0800

    rewrite
    Added mass encrypt and decrypt functionality.
    
    Changed functionality of the encrypt and decrypt script to handle original file metadata for a smoother decryption process.

--- src/decrypt.py
+++ src/decrypt.py
@@ -2,42 +2,94 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 import zstandard as zstd
+import glob
+import struct
 
-def decompress_file(compressed_data):
-    dctx = zstd.ZstdDecompressor()
-    decrompressed_data = dctx.decompress(compressed_data)
-    return decrompressed_data
 
-def decrypt_file(encrypted_file_path, key, output_file_path):
+def decrypt_file(encrypted_file_path, key, output_directory):
+    if not encrypted_file_path.endswith('.dun'):
+        print(f'Skipping non-encrypred file: {encrypted_file_path}')
+        return
+
     with open(encrypted_file_path, 'rb') as f:
-        iv = f.read(12)
-        file_content = f.read()
-        encrypted_data = file_content[:-16]
-        tag  = file_content[-16:]
+        file_contents = f.read()
+    iv = file_contents[:12]
+    encrypted_data = file_contents[12:-16]
+    tag  = file_contents[-16:]
 #        I would not recommend printing the key, iv, and tag in a production environment, but it is useful for debugging
 #        print(f"IV: {iv.hex()} Length: {len(iv)}")
 #        print(f"Tag: {tag.hex()} Length: {len(tag)}")
 #        print(f"Key: {key.hex()} Length: {len(key)}")
-
-    
     cipher = Cipher(algorithms.AES(key), modes.GCM(iv, tag), backend=default_backend())
     decryptor = cipher.decryptor()
 
-    decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
+    try:
+        decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
+    except Exception as e:
+            print(f'Failed to decrypt {encrypted_file_path}: {e}')
+            return
+    
+    filename_length = decrypted_data[0]
+    filename_bytes = decrypted_data[1:filename_length + 1]
+    original_filename = filename_bytes.decode()
 
-    decompressed_data = decompress_file(decrypted_data)
+    compressed_data = decrypted_data[filename_length +1:]
 
-    with open(output_file_path, 'wb') as out_file:
-        out_file.write(decompressed_data)
-    print(f"File decrupted and saved as {output_file_path}")
+    dctx = zstd.ZstdDecompressor()
+    decompressed_data = dctx.decompress(compressed_data)
+
+    output_file_path = os.path.join(output_directory, original_filename)
+    with open(output_file_path, 'wb') as f:
+        f.write(decompressed_data)
+    print(f"File decrypted and saved as {output_file_path}")
+
+def main():
+    print("Decryption Modes: Manual , Directory")
+    mode = input("What mode would you like to continue with? ").strip()
+
+
+    if mode.lower() in ('manual', 'man'):
+        file_path = input("Enter the path to the encrypted .dun file: ")
+        if not file_path.endswith('.dun'):
+            print('Invalid file type. Only .dun files can be decrypted')
+            return
+        
+        output_directory = input("Enter the output file path for the decryped file: ")
+        os.makedirs(output_directory, exist_ok=True)
+
+        image_path = input("Enter the key image: ")
+        from image_process import derive_key_from_image
+        key = derive_key_from_image(image_path)
+        decrypt_file(file_path, key, output_directory)
+
+
+    elif mode.lower() in ('directory', 'dir'):
+        directory = input('Enter the Directory containing .dun files: ').strip()
+        if not os.path.exists(directory):
+            print("Invalid directory.")
+            return
+
+        dun_files = glob.glob(os.path.join(directory, "*.dun"))
+        if not dun_files:
+             print('There are no .dun files in specified directory')
+             return
+        
+        output_directory = input("Enter the output directory: ").strip()
+        os.makedirs(output_directory, exist_ok=True)
+        image_path = input('Enter the path to the image used for decryption: ').strip()
+        from image_process import derive_key_from_image
+        key = derive_key_from_image(image_path)
+
+        for dun_file in dun_files:
+            if os.path.isfile(dun_file):
+                try:
+                    decrypt_file(dun_file, key, output_directory)
+                except Exception as e:
+                    print(f"there was an error decrypting {dun_file}: {e}")
+    else:
+        print(f'{mode} is an invalid mode')
+
+
 
 if __name__ == "__main__":
-    encrypted_file_path = input("Enter the path to the encrypted .dun file: ")
-    output_file_path = input("Enter the output file path for the decryped file: ")
-
-    image_path = input("enter the key image: ")
-    from image_process import derive_key_from_image
-    key = derive_key_from_image(image_path)
-
-    decrypt_file(encrypted_file_path, key, output_file_path)
-
+    main()
--- src/encrypt.py
+++ src/encrypt.py
@@ -1,49 +1,88 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import hashes, padding
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import os
+import struct
 import zstandard as zstd
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import glob
 
-def compress_file(file_path):
-    with open(file_path, 'rb') as f:
-        data = f.read()
-    cctx = zstd.ZstdCompressor()
-    compressed_data =cctx.compress(data)
-    return compressed_data
 
-def encrypt_file(file_path, key, output_path):
-    with open(file_path, 'rb') as f:
+
+def encrypt_file(input_file_path, key, output_path):
+    with open(input_file_path, 'rb') as f:
         file_data = f.read()
 
-    compressed_data = compress_file(file_path)
 
+    cctx = zstd.ZstdCompressor()
+    compressed_data = cctx.compress(file_data)
 
     iv = os.urandom(12)
 
+    original_filename = os.path.basename(input_file_path)
+    filename_bytes = original_filename.encode()
+    filename_length = len(filename_bytes)
+
+    file_metadata = struct.pack(f"B{filename_length}s", filename_length, filename_bytes)
+
+    pre_encryptdata = file_metadata + compressed_data
+
+
     cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
     encryptor = cipher.encryptor()
+    encrypted_data = encryptor.update(pre_encryptdata) + encryptor.finalize()
 
-    encrypted_data = encryptor.update(compressed_data) + encryptor.finalize()
+    encrypted_file_data = iv + encrypted_data + encryptor.tag
 
-    with open(output_path, 'wb') as output_file:
-        output_file.write(iv)
-        output_file.write(encrypted_data)
-        output_file.write(encryptor.tag)
+    with open(output_path, 'wb') as f:
+        f.write(encrypted_file_data)
 #        I would not recommend printing the key, iv, and tag in a production environment, but it is useful for debugging
 #        print(f"IV: {iv.hex()} Length: {len(iv)}")
 #        print(f"Tag: {encryptor.tag.hex()} Length: {len(encryptor.tag)}")
 #        print(f"Key: {key.hex()} Length: {len(key)}")
 
-    print(f"File encrypted and saved as {output_file}")
+    print(f"File encrypted and saved as {output_path}")
+
+
+def main():
+    print("Encryption Modes available: Manual, Directory")
+    mode = input("What mode would you like to continue with? ")
+    
+    if mode.lower() in ("man", "manual"):
+        file_path = input("Enter the path the the file to encrypt: ")
+        if file_path.endswith(".dun"):
+            print("Skipping files already encrypted.")
+            return
+        
+        encoutput_path = file_path + '.dun'
+
+        image_path = input("Enter the path to the image being used for encryption: ").strip()
+        from image_process import derive_key_from_image
+        key = derive_key_from_image(image_path)
+
+        encrypt_file(file_path, key, encoutput_path) 
+
+    elif mode.lower() in ('dir','directory'):
+        directory = input("Input the directory holding the files you want to encrypt: ").strip()
+        if not os.path.exists(directory):
+            print(f"{directory} is not a valid directory.")
+            return
+        files = glob.glob(os.path.join(directory, '*'))
+        if not files:
+            print(f'No files were found in {directory}')
+            return
+        image_path = input("Enter the path to the image being used for encryption").strip()
+        from image_process import derive_key_from_image
+        key = derive_key_from_image(image_path)
+
+        for file_path in files:
+            if os.path.isfile(file_path) and not file_path.endswith(".dun"):
+                encoutput_path = file_path + ".dun"
+                try:
+                    encrypt_file(file_path,key,encoutput_path)
+                except Exception as e:
+                    print(f'Error encrypting {file_path}: {e}')
+    else:
+        print(f"{mode} is an invalid mode.")
 
 
 if __name__ == "__main__":
-    file_path = input("Enter the path to the file to encrypt: ")
-    output_path = input("Enter the output file path ('filename'.dun): ")
-
-    image_path = input("Enter the image path to derive key: ")
-    from image_process import derive_key_from_image
-    key = derive_key_from_image(image_path)
-
-    encrypt_file(file_path, key, output_path)
+    main()
